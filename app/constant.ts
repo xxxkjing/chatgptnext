9389eb10ca9caf1a8551d14fca8dab60e740bb8b
MetaIllusion <xkjing@outlook.com>
2025-08-14 21:32:09 +0800

    g

--- app/constant.ts
+++ app/constant.ts
@@ -511,6 +511,8 @@ export const EXCLUDE_VISION_MODEL_REGEXES = [/claude-3-5-haiku-20241022/];
 const openaiModels = [
   // As of July 2024, gpt-4o-mini should be used in place of gpt-3.5-turbo,
   // as it is cheaper, more capable, multimodal, and just as fast. gpt-3.5-turbo is still available for use in the API.
+  "gemini-2.5-pro",
+  "gemini-2.5-flash",
   "gpt-4o",
   "gpt-5-mini",
   "gpt-5-chat-latest",
